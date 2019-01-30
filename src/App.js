ca4cab814f6c199c7fec9b62efe80be760fb7b00
cetiniz <zcetinic@quartermaster.house>
2019-01-29 20:01:00 -0500

    [AP Client] add - Added D3 as modular libary, added force graph, added API call and data transform, added force graph

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Network from './Network.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      nodes: null,
+      edges: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api').then(res => {
+      res.json().then(json => {
+        const edges = [];
+        const nodes = [];
+        const addedNodes = new Set();
+          json.forEach(node => {
+              const edge = {
+                source: node.From.Name,
+                target: node.To.Name,
+              };
+              edges.push(edge);
+              if (!addedNodes.has(node.From.Name)) {
+                  nodes.push(node.From);
+                  addedNodes.add(node.From.Name)
+              }
+            if (!addedNodes.has(node.To.Name)) {
+              nodes.push(node.To);
+              addedNodes.add(node.To.Name)
+            }
+          });
+        this.setState({ nodes, edges });
+      })
+    })
+  }
+
+  setNodeData = (nodes) => {
+    this.setState({ nodes });
+  }
+
   render() {
+    const { nodes, edges } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {nodes && (
+          <Network nodes={nodes} edges={edges}/>
+          )}
         </header>
       </div>
     );
---
+++ src/Network.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { select } from 'd3-selection';
+import { forceManyBody, forceSimulation, forceCenter, forceLink } from 'd3-force';
+
+class Network extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    const svg = select("svg"),
+      width = +svg.attr("width"),
+      height = +svg.attr("height");
+    const simulation = forceSimulation().nodes(this.props.nodes);
+    simulation.force("charge_force", forceManyBody())
+      .force("center_force", forceCenter(width / 2, height / 2));
+    const node = svg.append("g")
+        .attr("class", "nodes")
+        .selectAll("g")
+        .data(this.props.nodes)
+        .enter().append("g");
+    const circle = node
+        .append("circle")
+        .attr("r", 5)
+        .attr("fill", "red");
+    node.append("text")
+        .text(function(d) {
+          return d.Name;
+        })
+        .attr('x', 6)
+        .attr('y', 3);
+    node.append("title")
+        .text(function(d) { return d.name; });
+    const link_force =  forceLink(this.props.edges)
+        .id(function(d) { return d.Name; });
+    simulation.force("links", link_force);
+    const link = svg.append("g")
+        .attr("class", "links")
+        .selectAll("line")
+        .data(this.props.edges)
+        .enter().append("line")
+        .attr("stroke-width", 2);
+    simulation
+        .nodes(this.props.nodes)
+        .on("tick", () => this.tickActions(node, link) );
+  }
+
+  tickActions(node, link) {
+    node
+        .attr("transform", function(d) {
+          return "translate(" + d.x + "," + d.y + ")";
+        });
+    link
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+  }
+  render() {
+    return (
+      <svg width="960" height="600" />
+    );
+  }
+}
+
+export default Network;
+
