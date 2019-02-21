270b53864aadf21c400c87352ec0c386522e2873
cetiniz <zcetinic@quartermaster.house>
2019-02-21 01:19:02 -0500

    Add physics to D3

--- src/Network.js
+++ src/Network.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
+import * as d3 from 'd3';
 import { select } from 'd3-selection';
+import { drag } from 'd3-drag';
+import { zoom } from 'd3-zoom';
 import { forceManyBody, forceSimulation, forceCenter, forceLink } from 'd3-force';
 
 class Network extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   componentDidMount() {
     const svg = select("svg"),
       width = +svg.attr("width"),
       height = +svg.attr("height");
+      svg.style("border", "1px solid white")
     const simulation = forceSimulation().nodes(this.props.nodes);
     simulation.force("charge_force", forceManyBody())
       .force("center_force", forceCenter(width / 2, height / 2));
-    const node = svg.append("g")
+    const zoomWindow = svg.append("g")
+        .attr("class", "everything");
+    const node = zoomWindow.append("g")
         .attr("class", "nodes")
         .selectAll("g")
         .data(this.props.nodes)
@@ -28,19 +30,62 @@ class Network extends Component {
           return d.Name;
         })
         .attr('x', 6)
-        .attr('y', 3);
+        .attr('y', 3)
+        .style('font-size', 12)
+        .style('fill', 'white');
     node.append("title")
         .text(function(d) { return d.name; });
     const link_force =  forceLink(this.props.edges)
         .id(function(d) { return d.Name; });
     simulation.force("links", link_force);
-    const link = svg.append("g")
+    const link = zoomWindow.append("g")
         .attr("class", "links")
         .selectAll("line")
         .data(this.props.edges)
         .enter().append("line")
         .attr("stroke-width", 2);
+
+    const charge_force = forceManyBody()
+        .strength(-100);
+
+    //add drag capabilities
+    const drag_handler = drag()
+        .on("start", drag_start)
+        .on("drag", drag_drag)
+        .on("end", drag_end);
+
+    const zoom_handler = zoom()
+        .on("zoom", zoom_actions);
+
+    function drag_start(d) {
+      if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+      d.fx = d.x;
+      d.fy = d.y;
+    }
+
+//make sure you can't drag the circle outside the box
+    function drag_drag(d) {
+      d.fx = d3.event.x;
+      d.fy = d3.event.y;
+    }
+
+    function drag_end(d) {
+      if (!d3.event.active) simulation.alphaTarget(0);
+      d.fx = null;
+      d.fy = null;
+    }
+
+    //Zoom functions
+    function zoom_actions(){
+      zoomWindow.attr("transform", d3.event.transform)
+    }
+
+    zoom_handler(svg);
+    drag_handler(node);
+
     simulation
+        .force("charge_force", charge_force)
+        .force("links",link_force)
         .nodes(this.props.nodes)
         .on("tick", () => this.tickActions(node, link) );
   }
@@ -58,7 +103,7 @@ class Network extends Component {
   }
   render() {
     return (
-      <svg width="960" height="600" />
+        <svg width="960" height="600" />
     );
   }
 }
